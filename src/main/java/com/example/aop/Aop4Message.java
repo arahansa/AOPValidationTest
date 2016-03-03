08f584c6cc99af1226c3578f8c2e213b567c0df9
ararhansa <arahansa@naver.com>
2016-03-03 20:13:24 +0900

    add aop

---
+++ src/main/java/com/example/aop/Aop4Message.java
@@ -0,0 +1,6 @@
+package com.example.aop;
+
+
+public class Aop4Message {
+
+}
--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -2,21 +2,26 @@ package com.example.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.User;
+import com.example.service.HomeService;
 
 @RestController
 public class HomeController {
+	
+	@Autowired HomeService homeService;
 	
 	@RequestMapping("/home")
 	public String homeTest(@Valid User user, BindingResult result){
 		if(result.hasErrors()){
 			return "errors "+result;
 		}
-		return "hello world : "+user;
+		String msg = homeService.checkString("test");
+		return "hello world : "+user +" ,  msg :"+msg;
 	}
 
 }
